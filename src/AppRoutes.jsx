b0546047de4cb53638ad7853cf63e5ac4f358761
Eduardo <44625373+Eduardo-Virissimo@users.noreply.github.com>
2023-04-12 04:41:17 -0300

    update

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,22 +1,49 @@
+import React, { useContext } from "react";
+
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Navigate
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
 } from "react-router-dom";
 
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+import { AuthProvider, AuthContext } from "./contexts/auth";
 
 const AppRoutes = () => {
-    return(
-        <Router>
-            <Routes>
-                <Route exact path="/login" element={<LoginPage />}/>
-                <Route exact path="/" element={<HomePage />}/>
-            </Routes>
-        </Router>
-    )
-}
+  const Private = ({ children }) => {
+    const { authenticated, loading } = useContext(AuthContext);
 
-export default AppRoutes
+    if(loading){
+      return <div className="laoding">Carregando...</div>
+    }
+
+    if (!authenticated) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Private>
+                <HomePage />
+              </Private>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+};
+export default AppRoutes;
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+
+import { AuthContext } from "../../contexts/auth";
 
 import "./styles.css";
 
 const LoginPage = () => {
+  const { authenticated, login } = useContext(AuthContext);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("submit", { email, password });
+
+    login(email, password);
   };
 
   return (
